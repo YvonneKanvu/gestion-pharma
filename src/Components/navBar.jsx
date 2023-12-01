bb41982292425aa1e80894bcca6cd9df0c1d44bb
yvonne kanvu <yvkanvu@gmail.com>
2023-12-01 10:41:35 +0100

    create component button add a login features

--- src/Components/navBar.jsx
+++ src/Components/navBar.jsx
@@ -3,9 +3,9 @@ function NavBar(){
     return(
 <nav>
     <ul>
-      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/'>login</NavLink></li>
+      <li><NavLink to='/home'>Home</NavLink></li>
       <li><NavLink to='/about'>about</NavLink></li>
-      <li><NavLink to='/login'>login</NavLink></li>
       <li><NavLink to='/products'>products</NavLink></li>
       
     </ul>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+import Button from "../Components/button";
+
 function Login() {
     return(
+        <>
         <h1>Login</h1>
+    
+        <Button linkValue='/home' buttonValue='Connexion'/>
+        </>
     )
 }
 export default Login;
